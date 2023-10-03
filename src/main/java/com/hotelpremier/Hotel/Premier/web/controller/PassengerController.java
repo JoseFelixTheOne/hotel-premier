38427bea1b77e095898c641e3d786ecff4addf95
Dante <luisgonzalo152@gmail.com>
2023-10-03 18:07:12 -0500

    agregando pasajero

---
+++ src/main/java/com/hotelpremier/Hotel/Premier/web/controller/PassengerController.java
@@ -0,0 +1,9 @@
+package com.hotelpremier.Hotel.Premier.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/passengers")
+public class PassengerController {
+}
