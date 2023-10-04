b08e40218295030ff406fde651a224b5dd8e5a8b
hapastorv <alepastor1@hotmail.com>
2023-10-04 15:24:45 -0500

    Avance TipoUsuarioMenu

--- src/main/java/com/hotelpremier/Hotel/Premier/persistence/entity/TipoUsuarioMenu.java
+++ src/main/java/com/hotelpremier/Hotel/Premier/persistence/entity/TipoUsuarioMenu.java
@@ -25,4 +25,44 @@ public class TipoUsuarioMenu {
     @ManyToOne
     @JoinColumn(name = "id_menu", insertable = false, updatable = false)
     private Menu objMenu;
+
+    public int getIdMenuTipoUsuario() {
+        return idMenuTipoUsuario;
+    }
+
+    public void setIdMenuTipoUsuario(int idMenuTipoUsuario) {
+        this.idMenuTipoUsuario = idMenuTipoUsuario;
+    }
+
+    public int getIdTipoUsuario() {
+        return idTipoUsuario;
+    }
+
+    public void setIdTipoUsuario(int idTipoUsuario) {
+        this.idTipoUsuario = idTipoUsuario;
+    }
+
+    public int getIdMenu() {
+        return idMenu;
+    }
+
+    public void setIdMenu(int idMenu) {
+        this.idMenu = idMenu;
+    }
+
+    public TipoUsuario getObjTipoUsu() {
+        return objTipoUsu;
+    }
+
+    public void setObjTipoUsu(TipoUsuario objTipoUsu) {
+        this.objTipoUsu = objTipoUsu;
+    }
+
+    public Menu getObjMenu() {
+        return objMenu;
+    }
+
+    public void setObjMenu(Menu objMenu) {
+        this.objMenu = objMenu;
+    }
 }
