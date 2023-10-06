91b97618973f0ea0ca9c6d1b29ead4eab911b392
hapastorv <alepastor1@hotmail.com>
2023-10-06 11:39:44 -0500

    Agregando dependecia a SpringSecurity

--- src/main/java/com/hotelpremier/Hotel/Premier/web/config/WebSecurityConfig.java
+++ src/main/java/com/hotelpremier/Hotel/Premier/web/config/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.hotelpremier.Hotel.Premier.web.config;
 
-/*
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,9 +8,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
- */
 public class WebSecurityConfig{
-    /*
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -21,5 +18,4 @@ public class WebSecurityConfig{
                 .httpBasic(withDefaults());
         return http.build();
     }
-     */
 }
