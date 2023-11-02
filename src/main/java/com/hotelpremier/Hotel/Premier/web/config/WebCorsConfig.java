120a82ee40edf84ffb1806f75502a632481bdb3f
Nemo <torrestjfelix@gmail.com>
2023-11-01 19:52:47 -0500

    cambio final al mapping

--- src/main/java/com/hotelpremier/Hotel/Premier/web/config/WebCorsConfig.java
+++ src/main/java/com/hotelpremier/Hotel/Premier/web/config/WebCorsConfig.java
@@ -10,7 +10,7 @@ public class WebCorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/web/controller/**")
+                registry.addMapping("/**")
                         .allowedOrigins("http://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .maxAge(3600);
