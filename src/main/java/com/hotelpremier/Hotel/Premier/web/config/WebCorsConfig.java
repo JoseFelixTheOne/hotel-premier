d2b3c30e876046429e530bc423b72721c2623627
Nemo <torrestjfelix@gmail.com>
2023-11-01 19:43:23 -0500

    Preparando el cors config global

---
+++ src/main/java/com/hotelpremier/Hotel/Premier/web/config/WebCorsConfig.java
@@ -0,0 +1,21 @@
+package com.hotelpremier.Hotel.Premier.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebCorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("./../controller/")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .maxAge(3600);
+                WebMvcConfigurer.super.addCorsMappings(registry);
+            }
+        };
+    }
+}
