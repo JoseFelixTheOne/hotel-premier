07eed2e6b979f5a150bb665ad7d8dd4f6fe421a8
Nemo <torrestjfelix@gmail.com>
2023-11-06 13:33:40 -0500

    Avance prometedor

--- src/main/java/com/hotelpremier/Hotel/Premier/HotelPremierApplication.java
+++ src/main/java/com/hotelpremier/Hotel/Premier/HotelPremierApplication.java
@@ -2,12 +2,27 @@ package com.hotelpremier.Hotel.Premier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HotelPremierApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelPremierApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200")
+						.allowedMethods("*")
+						.allowedHeaders("*");
+			}
+		};
 	}
 
 }
