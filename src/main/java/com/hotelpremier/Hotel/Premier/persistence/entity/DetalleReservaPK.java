53fb7bbc9ea2fdb4feb91add8a71cec916f1469c
Nemo <torrestjfelix@gmail.com>
2023-11-18 22:31:27 -0500

    Detalle PK

---
+++ src/main/java/com/hotelpremier/Hotel/Premier/persistence/entity/DetalleReservaPK.java
@@ -0,0 +1,20 @@
+package com.hotelpremier.Hotel.Premier.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Embeddable
+@Getter
+@Setter
+public class DetalleReservaPK implements Serializable {
+    @Column(name = "id_detallereserva")
+    private Integer idDetalle;
+
+    @Column(name = "id_reserva")
+    private Integer idReserva;
+}
