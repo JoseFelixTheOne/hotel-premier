efdafed514b05dab42d1ea27c4f1b54b558c8ad2
hapastorv <alepastor1@hotmail.com>
2023-11-20 21:16:22 -0500

    Cambios realizados

--- src/main/java/com/hotelpremier/Hotel/Premier/persistence/TipoUsuarioMenuRepository.java
+++ src/main/java/com/hotelpremier/Hotel/Premier/persistence/TipoUsuarioMenuRepository.java
@@ -1,6 +1,5 @@
 package com.hotelpremier.Hotel.Premier.persistence;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
 import com.hotelpremier.Hotel.Premier.domain.UserTypeMenu;
 import com.hotelpremier.Hotel.Premier.domain.repository.UserTypeMenuRepository;
 import com.hotelpremier.Hotel.Premier.persistence.crud.TipoUsuarioMenuCrudRepository;
